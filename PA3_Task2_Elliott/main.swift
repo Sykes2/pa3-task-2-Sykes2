1c831382fc715a2f7ee65001c34ff1c3bc8af90f
Elliott, Jared Padilla <jelliott3@hk223-27711.local>
2018-09-19 21:58:56 -0700

    Initial Commit

---
+++ PA3_Task2_Elliott/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  PA3_Task2_Elliott
+//
+//  Created by Elliott, Jared Padilla on 9/19/18.
+//  Copyright © 2018 Jared Elliott. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
